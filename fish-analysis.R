d00301c583741314d93342be66e266b1f989a129
sc62 <mattch85@gmail.com>
2021-02-11 01:41:45 -0800

    change category "big" to "large"

--- fish-analysis.R
+++ fish-analysis.R
@@ -4,4 +4,4 @@ fish_data = read.csv("data/Gaeta_etal_CLC_data_1.csv")
 library(dplyr)
 
 fish_data_cat = fish_data %>% 
-  mutate(length_cat = ifelse(length > 300, "big", "small"))
+  mutate(length_cat = ifelse(length > 300, "large", "small"))
