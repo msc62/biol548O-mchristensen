04be40a2ed49eca446dcc3e4d3ef86a197919328
sc62 <mattch85@gmail.com>
2021-02-11 01:35:41 -0800

    Discovered more data is imminent. Append a "1" to original data csv to identify first dataset received and worked on. Update R script to reference new file name in data subdirectory.

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,6 +1,6 @@
 ##This is a script for comparing fish length and scale size. Data is imported from [Gaeta_etal_CLC_data.csv](https://lter.limnology.wisc.edu/sites/default/files/Gaeta_etal_CLC_data.csv). The fish lengths are provided in a unit of measure and this code creates a new column for size categories based on size. 
 
-fish_data = read.csv("data/Gaeta_etal_CLC_data.csv")
+fish_data = read.csv("data/Gaeta_etal_CLC_data_1.csv")
 library(dplyr)
 
 fish_data_cat = fish_data %>% 
