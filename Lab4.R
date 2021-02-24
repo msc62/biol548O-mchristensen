cb45d2b9cf5345f7f57962c28ea3848822e773d7
sc62 <mattch85@gmail.com>
2021-02-23 23:44:40 -0800

    Exercise 2 complete

--- Lab4.R
+++ Lab4.R
@@ -33,3 +33,27 @@ shrubs_5m <- filter(shrubs, height > "5")
 shrub_data_w_vols <- mutate(shrubs, volume=length*width*height)
 
 
+#Exercise 2
+##Create a new table of only species_id and date info
+species_date<-select(surveys,year,month,day,species_id)
+##Create a new table of with year, species_id and weight in kg
+surveys2<-select(surveys, year, species_id, weight)
+surveys3<-mutate(surveys2,weight2=weight/1000)
+surveys4<-na.omit(surveys3)
+
+##Select out only species "SH"
+surveys_SH <- filter(surveys4, species_id=="SH")
+
+#Get the total number of individual species
+surveys_byspecies <- group_by(surveys, species_id)
+surveys_byspeciescount <- summarize(surveys_byspecies, abundance = n())
+
+#Get total number of individuals in each species id in each year
+surveys_byspecieswithyear <- group_by(surveys, species_id, year)
+surveys_byspeciescount <- summarize(surveys_byspecieswithyear, abundance = n())
+
+#Determine mean mass of "D0" by year
+surveys_D0 <- filter(surveys, species_id == "D0")
+surveys_D0_by_year <- group_by(surveys_D0, year)
+D0_weight_by_year <- summarize(surveys_D0_by_year,
+                               avg_weight = mean(weight, na.rm = TRUE))
