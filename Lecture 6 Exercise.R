a8b0ca03ef1086a918c158e48060402419829961
sc62 <mattch85@gmail.com>
2021-03-02 16:23:32 -0800

    Finish Exercise 3

--- Lecture 6 Exercise.R
+++ Lecture 6 Exercise.R
@@ -1,1 +1,45 @@
-Lecture 6 Exercise
+##Lecture 6 Exercise
+library(stringr)
+install.packages("gapminder")
+gDat <- gapminder::gapminder
+str(gDat)
+
+(strings <- c("a", "ab", "acb", "accb", "acccb", "accccb"))
+grep("ac*b", strings, value = TRUE)
+grep("ac+b", strings, value = TRUE)
+grep("ac?b", strings, value = TRUE)
+grep("ac{2}b", strings, value = TRUE)
+grep("ac{2,}b", strings, value = TRUE)
+grep("ac{2,3}b", strings, value = TRUE)
+
+
+##Exercise 1
+
+##Write a line of code to find all countries with ee in Gapminder using quantifiers. Add, commit, and push.
+
+grep("ee", gDat$country, value = TRUE)
+
+##
+Write code to find all .R files in the main folder of your repository.
+
+Hint: use dir() to get a vector of all filenames
+
+
+Exercise 3
+
+gsub("(.*i.*)(land$)|(.*t.*)(land$)", "\\1LAND", gDat$country)
+
+##This the working one? 
+gsub("(i|t)(.*)(land$)","\\1LAND", unique(gDat$country))
+
+gsub("([it].*)land$","\\1LAND",unique(gDat$country))
+
+gsub("land","LAND",grep("[it].*land$",unique(gDat$country),v=T))
+
+
+##Exercise 4
+
+##Find continents in Gapminder with letter o in it.
+
+
+
