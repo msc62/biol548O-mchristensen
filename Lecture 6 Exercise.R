c101af97c5ed8e330ad993b96a9e8ced533bd208
sc62 <mattch85@gmail.com>
2021-03-03 01:00:55 -0800

    All exercises complete.

--- Lecture 6 Exercise.R
+++ Lecture 6 Exercise.R
@@ -19,13 +19,13 @@ grep("ac{2,3}b", strings, value = TRUE)
 
 grep("ee", gDat$country, value = TRUE)
 
-##
-Write code to find all .R files in the main folder of your repository.
+##Exercise 2 
+#Write code to find all .R files in the main folder of your repository.
+#Hint: use dir() to get a vector of all filenames
+files <- list.files(pattern = "\\.R$")
+print(files)
 
-Hint: use dir() to get a vector of all filenames
-
-
-Exercise 3
+##Exercise 3
 
 gsub("(.*i.*)(land$)|(.*t.*)(land$)", "\\1LAND", gDat$country)
 
@@ -40,6 +40,6 @@ gsub("land","LAND",grep("[it].*land$",unique(gDat$country),v=T))
 ##Exercise 4
 
 ##Find continents in Gapminder with letter o in it.
-
+grep("o", unique(gDat$continent), value = TRUE)
 
 
