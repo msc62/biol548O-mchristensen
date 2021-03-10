c4862dbef04bdd939690b23ad8d707344aeffbae
sc62 <mattch85@gmail.com>
2021-03-10 02:14:51 -0800

    Exercise 5 complete

--- Lecture 8.R
+++ Lecture 8.R
@@ -83,3 +83,29 @@ get_mass_from_length_by_name(10, "Stegosauria")
 get_mass_from_length_by_name(8, "Theropoda")
 get_mass_from_length_by_name(12, "Sauropoda")
 get_mass_from_length_by_name(13, "Anklyosauria")
+
+
+##Exercise 5
+#print the numbers 2 through 16.
+for (i in 2:16){
+  print(i)
+}
+
+#print the numbers 1 through 5 one line at a time multiplied by 3
+for (i in 1:5){
+  print(i*3)
+}
+
+##print out the name of each bird one line at a time.
+birds = c('robin', 'woodpecker', 'blue jay', 'sparrow')
+for (i in 1:length(birds)){
+  print(birds[i])
+}
+
+#store one area for each radius.
+radius <- c(1.3, 2.1, 3.5)
+areas <- vector(mode = "numeric", length = length(radius))
+for (i in 1:length(radius)){
+  areas[i] <- pi * radius[i] ^ 2
+}
+areas
