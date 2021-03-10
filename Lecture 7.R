3a0b6c67163ec7cd291c546830c2090fa52e0414
sc62 <mattch85@gmail.com>
2021-03-10 01:02:22 -0800

    Exercise 3 complete

--- Lecture 7.R
+++ Lecture 7.R
@@ -41,3 +41,23 @@ get_mass_from_length<- function(length, a, b){
 }
 
 get_mass_from_length(26, 214.44, 1.46)
+
+##Exercise 3 
+#Allowing a and b to be passed as arguments to get_mass_from_length() made the function more flexible, but for some types of dinosaurs we don't have specific values of a and b and so we have to use general values that can be applied to a number of different species.
+
+#Set default values for a and b of a = 39.9 and b = 2.6 (the average values from Seebacher 2001).
+
+
+get_mass_from_length<- function(length, a=39.9, b=2.6){
+  mass <- a * length ^ b
+  return(mass)
+}
+
+#estimate the mass of a Sauropoda (a = 214.44, b = 1.46) that is 22 m long
+get_mass_from_length(22, 214.44, 1.46)
+
+##estimate the mass of a dinosaur from an unknown taxonomic group that is 16m long
+get_mass_from_length(16)
+
+
+
